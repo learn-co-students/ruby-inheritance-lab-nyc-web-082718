29a3c72724c41d77d0f61ab3b592fae88ab219b6
github username <github email address>
2018-09-04 13:51:23 -0400

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,12 @@
+# 1. Define the `User` class such that a user can have a first and last name. You'll need both a setter and a getter for first and last name.
+
+class User
+
+  attr_accessor :first_name, :last_name
+
+  def initialize
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+end
